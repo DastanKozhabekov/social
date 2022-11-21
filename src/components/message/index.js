efd822828332772721f00b1154181ab49b51f409
DastanKozhabekov <kozhabekovdastan@gmail.com>
2022-11-21 14:11:52 +0600

    init

--- src/components/message/index.js
+++ src/components/message/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import './message.scss'
 import PropTypes from 'prop-types'
+import formatDistanceToNow from 'date-fns/formatDistanceToNow'
+
 
 const message = ({avatar, text, date, user}) => {
     return (
@@ -9,6 +11,15 @@ const message = ({avatar, text, date, user}) => {
             <div className="message__avatar">
                 <img src={avatar} alt={`Avatar ${user.name}`} />
             </div>
+            <div className="message__content">
+                <div className="message__bubble">
+                <p className="message__text">
+                    {text}
+                </p>
+                </div>
+                <span className="message__date">{date}</span>
+            </div>
+    
         </div>
         </>
     )
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -8,7 +8,7 @@ const Home = () => {
         <Message 
         avatar='https://sun9-8.userapi.com/impg/j-h9FWEUYs-R5Z2ft1ZD0V3jKXboluW6Gr0PMA/dzu-hWb5L_o.jpg?size=960x1280&quality=96&sign=6a373c56c7e87b9c24c6472894143774&type=album'
         text = 'Hello, world'
-        date = 'Sun Nov 20 2022 21:00:00'
+        date = 'Mon Nov 21 2022 13:58:56'
         />
         </>
     );
